3154dbf4de8bff4dbacf74a000fa934dd98d8a61
krutikpatel <meetkrutik@gmail.com>
2019-06-24 17:04:24 -0700

    Create romanToInt.java

---
+++ strings/romanToInt.java
@@ -0,0 +1,51 @@
+/*
+ Roman to Integer
+ 
+*/
+
+class Solution {
+    /*
+    -store conversion of each letter into int array
+    -Trick
+        if next elem > curr elem, that means we have iV or ix, 4 or 9 or others. then we have to reduce num
+    */
+    public int romanToInt(String s) {
+        int nums[]=new int[s.length()];
+        
+        for(int i=0;i<s.length();i++){
+            switch (s.charAt(i)){
+                case 'M':
+                    nums[i]=1000;
+                    break;
+                case 'D':
+                    nums[i]=500;
+                    break;
+                case 'C':
+                    nums[i]=100;
+                    break;
+                case 'L':
+                    nums[i]=50;
+                    break;
+                case 'X' :
+                    nums[i]=10;
+                    break;
+                case 'V':
+                    nums[i]=5;
+                    break;
+                case 'I':
+                    nums[i]=1;
+                    break;
+            }
+        }
+        
+        int sum=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]<nums[i+1])
+                sum-=nums[i];
+            else
+                sum+=nums[i];
+        }
+        return sum+nums[nums.length-1];
+        
+    }
+}
