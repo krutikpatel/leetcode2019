9a53d06bfd599160fd2cfa33ef2f816b33fdc8eb
krutikpatel <meetkrutik@gmail.com>
2019-06-26 16:42:40 -0700

    Create unique-email-addresses.java

---
+++ strings/unique-email-addresses.java
@@ -0,0 +1,52 @@
+/*
+Unique Email Addresses
+
+Every email consists of a local name and a domain name, separated by the @ sign.
+
+For example, in alice@leetcode.com, alice is the local name, and leetcode.com is the domain name.
+
+Besides lowercase letters, these emails may contain '.'s or '+'s.
+
+If you add periods ('.') between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name.  For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.  (Note that this rule does not apply for domain names.)
+
+If you add a plus ('+') in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered, for example m.y+name@email.com will be forwarded to my@email.com.  (Again, this rule does not apply for domain names.)
+
+It is possible to use both of these rules at the same time.
+
+Given a list of emails, we send one email to each address in the list.  How many different addresses actually receive mails? 
+
+*/
+
+/*
+Note:
+we can make char iterator till @ and make new string using StrinBuilder
+That would be faster than this
+*/
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        if(emails == null)
+            return 0;
+        
+        Set<String> set = new HashSet<>();
+        for(String email : emails){
+            set.add(cleanupEmail(email));
+        }
+        return set.size();
+    }
+    
+    private String cleanupEmail(String email){
+        int splitter = email.indexOf('@');  //Remember this utility
+        String id = email.substring(0,splitter);
+        String rest = email.substring(splitter);
+        
+        //remove +
+        if (id.contains("+")) {
+            id = id.substring(0,id.indexOf('+')); //we only care abt first +, anything after that is discarded anyways
+        }
+        
+        //remove .
+        id = id.replaceAll("\\.","");   //remember this utility and \\ for before .
+        
+        return id+rest;
+    }
+}
