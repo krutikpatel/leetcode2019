d4c4c84113317ff92d4f58e440e9a058822544a5
krutikpatel <meetkrutik@gmail.com>
2019-06-26 16:49:05 -0700

    Create add-binary.java

---
+++ strings/add-binary.java
@@ -0,0 +1,35 @@
+/*
+Add Binary
+
+Given two binary strings, return their sum (also a binary string).
+
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+*/
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        
+        int i = a.length() - 1, j = b.length() -1, carry = 0;
+        
+        while (i >= 0 || j >= 0) {  //keep going till either of string still exists.
+            int sum = carry;
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
