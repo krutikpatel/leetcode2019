4e58b188907a2cdce151042aedac1ac032bd71f5
krutikpatel <meetkrutik@gmail.com>
2019-07-27 13:08:36 -0700

    Create maximum-subarray.java

---
+++ dynamic_programming/maximum-subarray.java
@@ -0,0 +1,23 @@
+/*
+Maximum Subarray
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+*/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int sumSoFar = nums[0];
+        
+        for(int i=1;i<nums.length;i++){
+            sumSoFar = Math.max(nums[i], sumSoFar+nums[i]);//we pick local max at/upto index i
+            maxSum = Math.max(maxSum, sumSoFar); // Even if we picked nums[i] in last line, our max is still whatver max is so far..
+        }
+        return maxSum;
+    }
+}
