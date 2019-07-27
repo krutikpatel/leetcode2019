78bfdd9f8f2c19b498f8d9489f4dc75b341c2299
krutikpatel <meetkrutik@gmail.com>
2019-07-27 13:16:15 -0700

    Update maximum-subarray.java

--- dynamic_programming/maximum-subarray.java
+++ dynamic_programming/maximum-subarray.java
@@ -9,6 +9,12 @@ Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 */
+/*
+    Greedy cum DP
+    sumSoFar is kind of memoized for DP
+    
+    leetcode soln explains this as approach as well
+    */
 class Solution {
     public int maxSubArray(int[] nums) {
         int maxSum = nums[0];
