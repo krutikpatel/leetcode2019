b2cee014075c1bfc17d416599f6fbafcd30b23e9
krutikpatel <meetkrutik@gmail.com>
2019-11-05 13:27:42 -0800

    Update coin-change.java

--- dynamic_programming/coin-change.java
+++ dynamic_programming/coin-change.java
@@ -20,18 +20,20 @@ easily solvable with BFS too
 BFS used to give optimal solution
 
 */
-class Solution {
-    public int coinChange(int[] coins, int amount) {
+public int coinChange(int[] coins, int amount) {
         if (coins == null || coins.length == 0 || amount <= 0)
 		return 0;
 	    
+        //we try to fill ans till amount
         int[] dp = new int[amount + 1];
         dp[0] = 0;
         
         //bottom -up. we will go 0 -> n and fill dp for each num till amount
+        //since we start from 0, we wont have to worry about re-use of same coin (in same iteration)
         for(int i=1;i<=amount; i++){
             dp[i] = amount+1;   //Integer.MAX_VALUE; will create overflow when we do +1
             
+            //fin dp[i]
             //try each coin, and find min for dp[i]
             //we dont care if ans was found, if val is MAX, we will know ans was not found
             for(int j=0;j<coins.length;j++){
@@ -44,4 +46,3 @@ class Solution {
         
         return dp[amount] > amount ? -1:dp[amount];
     }
-}
