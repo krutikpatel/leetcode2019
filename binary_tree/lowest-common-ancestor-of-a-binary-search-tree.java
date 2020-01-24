47d82a73c87eed71cc2505eda564b7bfe0404b7d
krutikpatel <meetkrutik@gmail.com>
2020-01-23 23:25:59 -0800

    Update lowest-common-ancestor-of-a-binary-search-tree.java

--- binary_tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ binary_tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,5 +1,5 @@
 /*
-Lowest Common Ancestor of a Binary Search Tree
+Lowest Common Ancestor of a Binary Search Tree - good BST property lesson
 
 Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
 
