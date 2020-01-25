410277abb5e2050c989c6c0635d0fc374e115960
krutikpatel <meetkrutik@gmail.com>
2020-01-24 22:58:18 -0800

    Update lesson_inorder-successor-in-bst.java

--- binary_tree/lesson_inorder-successor-in-bst.java
+++ binary_tree/lesson_inorder-successor-in-bst.java
@@ -45,6 +45,18 @@ class Solution {
     /*
     Leverage BST property.
     iteration with conditions suites binary search, deciding whether go left or right
+    
+    /*
+    Leverage BST property.
+    iteration with conditions suites binary search, deciding whether go left or right
+    
+    Algo
+    -We start from the root, utilizing the property of BST:
+
+    -If current node's value is less than or equal to p's value, we know our answer must be in the right subtree.
+    -If current node's value is greater than p's value, current node is a candidate. Go to its left subtree to see if we can find a smaller one.
+    -If we reach null, our search is over, just return the candidate.
+
     */
     public TreeNode inorderSuccessor(TreeNode root, TreeNode target) {
         
