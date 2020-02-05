6f11da85133b6c9be3536ce1233c035719b9efcd
krutikpatel <meetkrutik@gmail.com>
2020-02-05 12:26:38 -0800

    Update minimum-window-substring.java

--- strings/minimum-window-substring.java
+++ strings/minimum-window-substring.java
@@ -37,68 +37,123 @@ class Solution {
     Advanced:
         that HashMap can be replaced with int[26] 
     */
+    /*
     public String minWindow(String s, String t) {
         if(t == null || s == null || t.length() == 0 || s.length() == 0)
             return "";
         
-        System.out.println("s.length() = "+s.length());
+        System.out.println("t.length() = "+t.length());
         
         //to maintain window state, and represent target to check against
-        HashMap<Character,Integer> target = new HashMap<>();
-        HashMap<Character,Integer> window = new HashMap<>();
+        HashMap<Character,Integer> target = new HashMap<>();//ONLY t's CHAR WILL HAVE ENTRY IN THIS MAP
+        HashMap<Character,Integer> window = new HashMap<>();//ONLY t's CHAR WILL HAVE ENTRY IN THIS MAP
+        String ret="";
         
-        //fit target map
-        for(char c: t.toCharArray()){
-            int currCount = target.getOrDefault(c,0);   //Good utility method of map
-            target.put(c,currCount+1);
-        }        
-        
-        //window two pointers
-        int i=0,j=0;
-        int matchedCharSoFarCount = 0;  //done chars
-        int minLen = Integer.MAX_VALUE;
-        String min = "";
-        
-        for(j=0;j<s.length();j++){    //i only moves conditionally
-            char c = s.charAt(j);
-                
-            //update window count
-            int currCount = window.getOrDefault(c,0);   //Good utility method of map
-            window.put(c,currCount+1);                
-
-            //update matchedCharSoFarCount
-            if(window.get(c) == target.get(c)){  //IMP - Only update once. Think 'a' count matched, if we find more 'a' we must not incement matchedCharSoFarCount
-                matchedCharSoFarCount++;
-                System.out.println("matchedCharSoFarCount = "+matchedCharSoFarCount);
-            }             
-            
-            //if found target, shrink from left until invalid
-            while(matchedCharSoFarCount == target.size() && i<=j){
-
-                //since we found target, record min
-                if(matchedCharSoFarCount == target.size()){
-                    if(j-i+1 < minLen){
-                        minLen = j-i+1;
-                        min = s.substring(i,j+1);                            
-                        System.out.println("min = "+min);
-                    }
-                }
-
-                //remove i'th char - slide from left
-                char ci = s.charAt(i);
-                //NOTE : only update window if c is in target, otherwise just i++
-                if(target.containsKey(ci)){                                               
-                    window.put(ci, window.get(ci)-1); 
-
-                    //update matchedCharSoFarCount
-                    if(window.get(ci) < target.get(ci)){ //if we have more chars than reqd, we are still good. otherwise only we need update
-                        matchedCharSoFarCount--;
-                    }
-                }
-                i++;
-            }                 
+        //prepare target
+        for(char c : t.toCharArray()) {
+            target.put(c, target.getOrDefault(c,0)+1);
         }
         
-        return min;
+        int min=s.length();
+        
+        //done chars
+        int done = 0;
+        
+        //window
+        int i=0;        
+        for(int j=0;j<s.length();j++) {
+            if(!target.containsKey(s.charAt(j))) {
+                continue;
+            }
+                       
+            //add to window
+            window.put(s.charAt(j), window.getOrDefault(s.charAt(j),0) + 1);
+            
+            //increase done count
+            done++;
+            
+            //if done == all, slide from left till not done
+            //while slide ONLY till its not done by one char
+            while(done == t.length()) {
+                //calc min
+                min = Math.min(min,j-i+1);
+                ret = s.substring(i,j+1);
+                
+                System.out.println(ret);
+                
+                //slide - 
+                problem in this logic and i++ 
+                if(target.containsKey(s.charAt(i)) ) { //window char count must not go beyond target
+                    
+                    if(window.get(s.charAt(i)) >= target.get(s.charAt(i))) {
+                        window.put(s.charAt(i), window.get(s.charAt(i)) - 1);
+                        done--;    
+                    }                                        
+                }
+                i++;
+            }
+        }
+        
+        return ret;
+    }
+    */
+    public String minWindow(String s, String t) {
+        // corner case
+        if(s == null || t == null || s.length() == 0 || t.length() == 0 || s.length() < t.length()) return "";
+        
+        // track min indices so that we can trturn min substring
+        int minLeft = 0;
+        int minRight = 0;
+        
+        //track min substring len
+        int min = s.length();
+        boolean flag = false;
+        
+        //record state of pattern in map. then dec/inc as we go thru each char 
+        Map<Character, Integer> map = new HashMap<>();
+        
+        //this tells us when we have match and till how long we have match when we slide window to squeeze
+        int count = t.length(); // the number of characters that I need to match
+        for(char c : t.toCharArray()) 
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        
+        // unfixed sliding window, 2 pointers
+        int i = 0;
+        int j = 0;
+        while(j < s.length()){
+            char c = s.charAt(j);
+            if(map.containsKey(c)){
+                map.put(c, map.get(c) - 1);//Step1: keep decrementing regardless/ we will take care of -ve vals, while squeezin window. 
+                if(map.get(c) >= 0) // we matched one char so count--. But since we allow -ve vals, the dont count in match
+                    count--; 
+            }
+            
+            // if found a susbtring - we need to squeeze from left TILL its no longer matched
+            //IMP: once we ahve match, all entries in map will be 0 or negative. once any entry is 1, no loonger match
+            while(count == 0 && i <= j){
+                // update global min
+                flag = true;
+                int curLen = j + 1 - i;
+                
+                //record min and indices
+                if(curLen <= min){
+                    minLeft = i;
+                    minRight = j;
+                    min = curLen;
+                }
+                
+                // shrink left pointer
+                char leftC = s.charAt(i);
+                if(map.containsKey(leftC)){
+                    map.put(leftC, map.get(leftC) + 1);
+                    if(map.get(leftC) >= 1) 
+                        count++;//Step2 - when entry goes >=1. our string/window dont match anymore
+                }
+                i++;
+            } 
+            j++;
+        }
+        
+        return flag == true ? s.substring(minLeft, minRight + 1): "";
     }
 }
