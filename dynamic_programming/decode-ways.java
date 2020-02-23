43b3560604e5fa09376141075158c9e416b55200
krutikpatel <meetkrutik@gmail.com>
2020-02-22 20:07:51 -0800

    Update decode-ways.java

--- dynamic_programming/decode-ways.java
+++ dynamic_programming/decode-ways.java
@@ -83,18 +83,19 @@ class Solution {
         dp[0] = s.charAt(0) != '0' ? 1 : 0;
         
         for(int i = 1; i < n; i++) {
-            int first = Integer.valueOf(s.substring(i, i+1));
-            int second = Integer.valueOf(s.substring(i-1, i+1));
-
+            int first = Integer.valueOf(s.substring(i, i+1));   //1th char
+            int second = Integer.valueOf(s.substring(i-1, i+1));//0,1 chars
+            
             //consider curr one char
-            if(first >= 1 && first <= 9) {
-                dp[i] += dp[i-1];
+            if(first!=0){
+                //continue same sentense/way
+                dp[i] = dp[i-1];
             }
             
             //consider curr and prev char to form a-z if possible
             if(second >= 10 && second <= 26) {
                 if(i>=2)
-                    dp[i] += dp[i-2];
+                    dp[i] = dp[i] + dp[i-2];//we found another way to create string at ith position.
                 else
                     dp[i] += 1;
                                 
