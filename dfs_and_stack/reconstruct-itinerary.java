42bb318bd3f9888af22a1f9d8f8a802b76bdb791
krutikpatel <meetkrutik@gmail.com>
2020-02-24 16:07:22 -0800

    Update reconstruct-itinerary.java

--- dfs_and_stack/reconstruct-itinerary.java
+++ dfs_and_stack/reconstruct-itinerary.java
@@ -33,8 +33,28 @@ class Solution {
     -is backtracking involved?
     
     -adding to result in correct order, since we are doing DFS recursion
-    */  
     
+    -Imp - we cant blindly proceed to next node without having actual route. it may happen in last method
+    
+    -----------------------------------
+    Soultion is Hierholzer's Algorithm
+    its not normal DFS
+    -----------------------------------
+    Algo notes:
+    The point that we got stuck would be the last airport that we visit. And then we follow the visited vertex (airport) backwards, we would obtain the final itinerary.
+
+=>Actually, we could consider the algorithm as the postorder DFS (Depth-First Search) in a directed graph, from a fixed starting point.
+
+=>As we know that, each input is guaranteed to have a solution. Therefore, the task of the problem can be interpreted as that given a list of flights (i.e. edges in graph), we should find an order to use each flight once and only once.
+
+=>In the resulted path, before we visit the last airport (denoted as V), we can say that we have already used all the rest flights, i.e. if there is any flight starting from V, then we must have already taken that before.
+
+=>Or to put it another way, before adding the last airport (vertex) in the final path, we have visited all its outgoing vertex.
+=>Before adding an airport into the final itinerary, we must first visit all its outgoing neighbor vertex.
+
+
+    */  
+   
     Map<String, PriorityQueue<String>> flights;//if there are 2 dest from same src, arrange them alphabetically
     LinkedList<String> path;
 
@@ -56,6 +76,48 @@ class Solution {
         while (dests != null && !dests.isEmpty()) {
             dfs(dests.poll());
         }            
+        //algorithm gives path in reve
         path.addFirst(departure);//imp - because its DFS, first one will get added last(recursive calls)
     }
+    
+    public void dfs2(String departure, List<String> sofar) {
+        PriorityQueue<String> dests = flights.get(departure);
+        while (dests != null && !dests.isEmpty()) {
+            dfs(dests.poll());
+        }            
+        path.addFirst(departure);//imp - because its DFS, first one will get added last(recursive calls)
+    }
+
+    /*
+    normal DFS - leads to routes that do not exist, because in loop, we go to all neighbors regardless of path existence
+    
+    HashMap<String, PriorityQueue<String>> graph;
+    public List<String> findItinerary(List<List<String>> tickets) {
+        //create adj list 
+        graph = new HashMap<>();
+        for(List<String> route : tickets){
+            PriorityQueue<String> curr = graph.getOrDefault(route.get(0),new PriorityQueue<>((a,b) -> a.compareTo(b)) );
+            curr.offer(route.get(1));
+            graph.put(route.get(0), curr);
+        }
+
+        List<String> ret = new ArrayList<>();
+        ret.add("JFK");
+        dfs("JFK",graph, ret);
+        return ret;
+    }
+
+    private void dfs(String from, HashMap<String, PriorityQueue<String>> graph, List<String> sofar){
+        String curr = from;//sofar.get(sofar.size()-1);
+        PriorityQueue<String> pq = graph.get(curr);
+        
+        while(pq!= null && !pq.isEmpty()){
+            String next = pq.poll();
+            //if(graph.get(next) != null){
+                sofar.add(next);
+                dfs(next,graph, sofar);
+            //}
+        }
+    }
+*/
 }
