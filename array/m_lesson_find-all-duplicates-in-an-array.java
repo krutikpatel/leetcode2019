4a1ad828be5e9bdeb52ad0fcdab0c4d7c7178f33
krutikpatel <meetkrutik@gmail.com>
2021-10-26 18:08:11 -0700

    Update m_lesson_find-all-duplicates-in-an-array.java

--- array/m_lesson_find-all-duplicates-in-an-array.java
+++ array/m_lesson_find-all-duplicates-in-an-array.java
@@ -46,6 +46,26 @@ class Solution {
     }
 }
 /*
+Simpler loop twice solution:
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+
+        for (int num : nums) {
+            nums[Math.abs(num) - 1] *= -1;
+        }
+
+        for (int num : nums) {
+            if (nums[Math.abs(num) - 1] > 0) {
+                ans.add(Math.abs(num));
+                nums[Math.abs(num) - 1] *= -1;
+            }
+        }
+
+        return ans;
+    }
+    
+*/
+/*
 Related problem:
 https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
     public List<Integer> findDisappearedNumbers(int[] nums) {
