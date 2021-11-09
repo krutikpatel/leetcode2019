0d9cd1e055df88f8912c290ef83f92cb50b57243
krutikpatel <meetkrutik@gmail.com>
2021-11-08 17:53:50 -0800

    Update and rename logger-rate-limiter.java to m_logger-rate-limiter.java

--- hashmap/m_logger-rate-limiter.java
+++ hashmap/m_logger-rate-limiter.java
@@ -1,4 +1,4 @@
-/*
+/* medium
 Logger Rate Limiter
 
 Design a logger system that receive stream of messages along with its timestamps, each message should be printed if and only if it is not printed in the last 10 seconds.
