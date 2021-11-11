4f026ecfe2c97d33b9e18722ac6cbd3ed0faaa75
krutikpatel <meetkrutik@gmail.com>
2021-11-10 22:46:16 -0800

    Update m_find-minimum-in-rotated-sorted-array.java

--- binary_search/m_find-minimum-in-rotated-sorted-array.java
+++ binary_search/m_find-minimum-in-rotated-sorted-array.java
@@ -49,4 +49,21 @@ class Solution {
         
         return nums[l];
     }
+    
+    /*
+    To handle duplicates
+    
+    public int findMin(int[] nums) {
+        int left=0;
+        int right=nums.length-1;
+        while(left<right) {
+            int mid = left+(right-left)/2;
+            if(nums[mid]>nums[right]) left=mid+1;
+            else if(nums[mid]<nums[left]) right=mid;
+            else right--;
+        }
+        return nums[left];
+    }
+
+    */
 }
