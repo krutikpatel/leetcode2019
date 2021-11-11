7eaaa6be012f5c691530a0e8628dba14f3ae2727
krutikpatel <meetkrutik@gmail.com>
2021-11-10 16:07:07 -0800

    Update and rename find-k-closest-elements.java to m_find-k-closest-elements.java

--- binary_search/m_find-k-closest-elements.java
+++ binary_search/m_find-k-closest-elements.java
@@ -1,4 +1,4 @@
-/*
+/* medium
 Find K Closest Elements
 
 Given a sorted array, two integers k and x, find the k closest elements to x in the array. The result should also be sorted in ascending order. If there is a tie, the smaller elements are always preferred.
