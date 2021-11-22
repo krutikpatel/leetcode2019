526b405812aab2fcdcb12089821bbd972dac3c72
krutikpatel <meetkrutik@gmail.com>
2021-11-22 14:49:54 -0800

    Update and rename palindromic-substrings.java to m_palindromic-substrings.java

--- strings/m_palindromic-substrings.java
+++ strings/m_palindromic-substrings.java
@@ -1,4 +1,4 @@
-/*
+/* medium
 Palindromic Substrings
 Given a string, your task is to count how many palindromic substrings in this string.
 The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
