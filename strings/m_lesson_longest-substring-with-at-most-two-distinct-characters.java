0a7e2cd37806b44e6d1aad7108d528a8e779c29c
krutikpatel <meetkrutik@gmail.com>
2021-11-28 12:41:09 -0800

    Update m_lesson_longest-substring-with-at-most-two-distinct-characters.java

--- strings/m_lesson_longest-substring-with-at-most-two-distinct-characters.java
+++ strings/m_lesson_longest-substring-with-at-most-two-distinct-characters.java
@@ -33,7 +33,40 @@ class Solution {
     Lesson: for sliding window, dont get ambitious and greedy, while sliding. Only slide minimum, conservatively.    
     
     */
-    public int lengthOfLongestSubstringTwoDistinct(String s) {
+  public int lengthOfLongestSubstringTwoDistinct(String s) {
+    int n = s.length();
+    if (n < 3) 
+        return n;
+
+    // sliding window left and right pointers
+    int left = 0;//only use of left ptr is to cal len of substring. not for remove char from left side.
+    int right = 0;
+    
+    // hashmap character -> its rightmost position
+    // in the sliding window
+    HashMap<Character, Integer> hashmap = new HashMap<Character, Integer>();
+
+    int max_len = 2;
+    while (right < n) {
+      // when the slidewindow contains less than 3 characters
+      hashmap.put(s.charAt(right), right++);
+
+      // slidewindow contains 3 characters
+      if (hashmap.size() == 3) {
+        // delete the leftmost character
+        int del_idx = Collections.min(hashmap.values());
+        hashmap.remove(s.charAt(del_idx));
+        
+        // move left pointer of the slidewindow
+        left = del_idx + 1;
+      }
+
+      max_len = Math.max(max_len, right - left);
+    }
+    return max_len;
+  }
+//////////////////////////////////
+    public int lengthOfLongestSubstringTwoDistinctOld(String s) {
         //char and latest(right most) index for that char
         HashMap<Character,Integer> map = new HashMap<>();
         int max=0;
