ced88ea4338a852232f13f1f4f63a6a80b5d38a5
krutikpatel <meetkrutik@gmail.com>
2021-11-28 12:44:54 -0800

    Update m_lesson_longest-substring-with-at-most-two-distinct-characters.java

--- strings/m_lesson_longest-substring-with-at-most-two-distinct-characters.java
+++ strings/m_lesson_longest-substring-with-at-most-two-distinct-characters.java
@@ -66,6 +66,29 @@ class Solution {
     return max_len;
   }
 //////////////////////////////////
+//with map<char,freq>
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        int start = 0, end = 0, counter = 0, len = 0;
+        while(end < s.length()){
+            char c = s.charAt(end);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            if(map.get(c) == 1) counter++;//new char
+            end++;
+            while(counter > 2){
+                char cTemp = s.charAt(start);
+                map.put(cTemp, map.get(cTemp) - 1);
+                if(map.get(cTemp) == 0){
+                    counter--;
+                }
+                start++;
+            }
+            len = Math.max(len, end-start);
+        }
+        return len;
+    }
+    
+////////////////
     public int lengthOfLongestSubstringTwoDistinctOld(String s) {
         //char and latest(right most) index for that char
         HashMap<Character,Integer> map = new HashMap<>();
