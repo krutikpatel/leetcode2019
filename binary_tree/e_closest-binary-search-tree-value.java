0d1790132289f116be7f74c9385effbad8df4414
krutikpatel <meetkrutik@gmail.com>
2021-11-30 17:23:17 -0800

    Update and rename closest-binary-search-tree-value.java to e_closest-binary-search-tree-value.java

--- binary_tree/e_closest-binary-search-tree-value.java
+++ binary_tree/e_closest-binary-search-tree-value.java
@@ -1,4 +1,4 @@
-/*
+/* easy
 Closest Binary Search Tree Value
 DescriptionHintsSubmissionsDiscussSolution
 Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
