84c623767a92321a3880a76f731046053b2d585b
krutikpatel <meetkrutik@gmail.com>
2022-03-25 10:58:37 -0700

    Update m_lesson_3sum-closest.java

--- array/m_lesson_3sum-closest.java
+++ array/m_lesson_3sum-closest.java
@@ -10,7 +10,46 @@ Given array nums = [-1, 2, 1, -4], and target = 1.
 The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 */
 class Solution {
-    public int threesumclosest(int[] nums, int t) {
+    public int threeSumClosest(int[] nums, int target) {
+        if(nums == null || nums.length < 3)
+            return 0;
+        
+        //Imp operation for int array
+        Arrays.sort(nums);
+        
+        int closest = nums[0] + nums[1] + nums[2]; //numsInteger.MAX_VALUE; wont work
+        
+        /*
+        take each elem : x, and find two-sum for other two elem = target-x
+        */
+        for(int i = 0;i<nums.length-2;i++){                        
+
+            //find two sum = target -x
+            //standard two sum one pass sweep
+            int l = i+1;
+            int r = nums.length-1;
+            int local_target = target - nums[i];
+            
+            while(l<r){
+                int currSum = nums[l] + nums[r] + nums[i];//easier to call them a,b,c for simplicity
+                
+                //track min difference from target
+                if(Math.abs(target - currSum) < Math.abs(target - closest))
+                    closest = currSum;
+                   
+                if(nums[l] + nums[r] < local_target){                    
+                    l++;
+                } else if(nums[l] + nums[r] > local_target){
+                    r--;
+                }else                         
+                    return target;                    
+            }
+        }
+        
+        return closest;
+    }
+    ////
+    public int threesumclosest2(int[] nums, int t) {
         //int ret = Integer.MAX_VALUE;
         if(nums == null || nums.length < 3)
             return 0;
