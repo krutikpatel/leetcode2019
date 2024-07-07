edb0adadadf42daf18b3b3725a39c5b7aaaf76aa
krutikpatel <meetkrutik@gmail.com>
2024-07-07 11:53:03 -0700

    Update permutations-ii.java

--- backtracking/permutations-ii.java
+++ backtracking/permutations-ii.java
@@ -15,6 +15,40 @@ Output:
 ]
 */
 class Solution {
+  //no sorting needed here
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums==null || nums.length==0) { return ans; }
+        permute(ans, nums, 0);
+        return ans;
+    }
+    
+    private void permute(List<List<Integer>> ans, int[] nums, int index) {
+        if (index == nums.length) { 
+            List<Integer> temp = new ArrayList<>();
+            for (int num: nums) { temp.add(num); }
+            ans.add(temp);
+            return;
+        }
+      
+        //use set for each iteration to manage visited nodes
+        Set<Integer> appeared = new HashSet<>();
+        for (int i=index; i<nums.length; ++i) {
+            //if not used
+            if (appeared.add(nums[i])) {
+                swap(nums, index, i);
+                permute(ans, nums, index+1);
+                swap(nums, index, i);
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int save = nums[i];
+        nums[i] = nums[j];
+        nums[j] = save;
+    }
+  
     /*
     Ref:
     https://leetcode.com/problems/combination-sum/discuss/16502/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)
