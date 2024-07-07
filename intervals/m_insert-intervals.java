517f9f4b058d259f1a34bb0eeaf18d77d5f9d41e
krutikpatel <meetkrutik@gmail.com>
2024-07-07 12:48:19 -0700

    Create m_insert-intervals.java

---
+++ intervals/m_insert-intervals.java
@@ -0,0 +1,49 @@
+/*
+https://leetcode.com/problems/insert-interval/description/
+
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+*/
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new LinkedList<>();
+                
+        int i = 0;
+        // Step-1 - add all the intervals ending before newInterval starts
+        while (i < intervals.length && intervals[i][1] < newInterval[0])
+            result.add(intervals[i++]);
+        
+        // Step-2 merge all overlapping intervals to one considering newInterval
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval = new int[]{
+                    Math.min(newInterval[0], intervals[i][0]),
+                    Math.max(newInterval[1], intervals[i][1])
+                };
+            i++;
+        }
+        result.add(newInterval); // add the union of intervals we got
+        
+        // Step-3 add all the rest
+        while (i < intervals.length) 
+            result.add(intervals[i++]);
+        
+        return result.toArray(new int[result.size()][2]);
+    }    
+}
